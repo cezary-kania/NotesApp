055c2709ac102c0eea987d3e8b049a5b40437a5d
cezary-kania <cezary.kaniadev@gmail.com>
2021-02-08 15:27:25 +0100

    Add note entity

---
+++ src/NotesApp.Domain/Entities/Note.cs
@@ -0,0 +1,65 @@
+using System;
+using NotesApp.Domain.Enums;
+
+namespace NotesApp.Domain.Entities
+{
+    public class Note
+    {
+        public Guid Id { get; protected set; }
+        public string Title { get; protected set; }
+        public string Content { get; protected set; }
+        public DateTime Created { get; protected set; }
+        public DateTime Modified { get; protected set; }
+        public string Status { get; protected set; }
+        public int VersionNo { get; protected set; }
+        public Guid InitialNoteId { get; protected set; }
+        protected Note()
+        {
+        }
+
+        protected Note(Guid noteId, string title, string content) 
+        {
+            Id = Guid.NewGuid(); // TODO: replace assignment with given noteId
+            SetTitle(title);
+            SetContent(content);
+            SetStatus(NoteStatus.Created);
+            VersionNo = 1;
+            Created = DateTime.UtcNow;
+            Modified = DateTime.UtcNow;
+        }
+
+        public void SetTitle(string title)
+        {
+            if(string.IsNullOrWhiteSpace(title)) throw new Exception("Title can not be empty.");
+        }
+
+        public void SetContent(string content)
+        {
+            if(string.IsNullOrWhiteSpace(content)) throw new Exception("Content can not be empty.");
+        }
+        public void MarkAsModified()
+            => Modified = DateTime.UtcNow;
+
+        public void SetStatus(string newStatus)
+            => Status = newStatus;
+
+        public Note CreateNewNote(Guid noteId, string title, string content)
+            => new Note(noteId, title, content);
+
+        public Note CreateNoteCopy(Guid noteId, Note note, string title, string content)
+        {
+            var noteCopy = new Note {
+                Id = noteId,
+                VersionNo = ++note.VersionNo,
+                InitialNoteId = (note.InitialNoteId == null) ? note.Id : note.InitialNoteId,
+                Created = note.Created,
+                Modified = DateTime.UtcNow
+            };
+            noteCopy.SetStatus(NoteStatus.Created);
+            noteCopy.SetTitle(title);
+            noteCopy.SetContent(content);
+            return noteCopy;
+        } 
+
+    }
+}
---
+++ src/NotesApp.Domain/Enums/NoteStatus.cs
@@ -0,0 +1,9 @@
+namespace NotesApp.Domain.Enums
+{
+    public static class NoteStatus
+    {
+        public static string Created => "Created";
+        public static string Modified => "Modified";
+        public static string Deleted => "Deleted";
+    }
+}
