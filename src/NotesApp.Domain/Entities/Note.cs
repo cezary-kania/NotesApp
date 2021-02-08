3e3db578e127f882761c43d68ab3e61faaea446a
cezary-kania <cezary.kaniadev@gmail.com>
2021-02-08 18:04:22 +0100

    Change note versioning system

--- src/NotesApp.Domain/Entities/Note.cs
+++ src/NotesApp.Domain/Entities/Note.cs
@@ -1,65 +1,46 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using NotesApp.Domain.Enums;
 
 namespace NotesApp.Domain.Entities
 {
     public class Note
     {
+        public ISet<NoteVersion> _versions = new HashSet<NoteVersion>();
         public Guid Id { get; protected set; }
-        public string Title { get; protected set; }
-        public string Content { get; protected set; }
         public DateTime Created { get; protected set; }
         public DateTime Modified { get; protected set; }
-        public string Status { get; protected set; }
-        public int VersionNo { get; protected set; }
-        public Guid PrevVersionNoteId { get; protected set; }
+        public IEnumerable<NoteVersion> Versions
+        {
+            get => _versions;
+            set => _versions = new HashSet<NoteVersion>(value);
+        }
         protected Note()
         {
         }
-
-        protected Note(Guid noteId, string title, string content) 
+        protected Note(Guid noteId) 
         {
-            Id = Guid.NewGuid(); // TODO: replace assignment with given noteId
-            SetTitle(title);
-            SetContent(content);
-            SetStatus(NoteStatus.Created);
-            VersionNo = 1;
+            Id = noteId;
             Created = DateTime.UtcNow;
             Modified = DateTime.UtcNow;
         }
 
-        public void SetTitle(string title)
-        {
-            if(string.IsNullOrWhiteSpace(title)) throw new Exception("Title can not be empty.");
-        }
-
-        public void SetContent(string content)
-        {
-            if(string.IsNullOrWhiteSpace(content)) throw new Exception("Content can not be empty.");
-        }
-        public void MarkAsModified()
+        public void UpdateModifyTime()
             => Modified = DateTime.UtcNow;
+        
+        public Note CreateNewNote(Guid noteId)
+            => new Note(noteId);
 
-        public void SetStatus(string newStatus)
-            => Status = newStatus;
-
-        public Note CreateNewNote(Guid noteId, string title, string content)
-            => new Note(noteId, title, content);
-
-        public Note CreateNoteCopy(Guid noteId, Note note, string title, string content)
+        public void AddVersion(string title, string content) 
         {
-            var noteCopy = new Note {
-                Id = noteId,
-                VersionNo = ++note.VersionNo,
-                PrevVersionNoteId = note.Id,
-                Created = note.Created,
-                Modified = DateTime.UtcNow
-            };
-            noteCopy.SetStatus(NoteStatus.Created);
-            noteCopy.SetTitle(title);
-            noteCopy.SetContent(content);
-            return noteCopy;
-        } 
-
+            var versionNo = _versions.Count + 1;
+            var newVersion = new NoteVersion(Id, versionNo, title, content);
+            _versions.Add(newVersion);
+        }
+        
+        public NoteVersion GetLastVersion()
+            => _versions.OrderByDescending(version => version.VersionNo)
+                        .First();
     }
 }
---
+++ src/NotesApp.Domain/Entities/NoteVersion.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace NotesApp.Domain.Entities
+{
+    public class NoteVersion
+    {
+        public Guid NoteId { get; protected set; }
+        public int VersionNo { get; protected set; }
+        public string Title { get; protected set; }
+        public string Content { get; protected set; }
+
+        public NoteVersion(Guid noteId, int versionNo, string title, string content)
+        {
+            NoteId = noteId;
+            VersionNo = versionNo;
+            SetTitle(title);
+            SetContent(content);
+        }
+        public void SetTitle(string title)
+        {
+            if(string.IsNullOrWhiteSpace(title)) throw new Exception("Title can not be empty.");
+        }
+
+        public void SetContent(string content)
+        {
+            if(string.IsNullOrWhiteSpace(content)) throw new Exception("Content can not be empty.");
+        }
+    }
+}
