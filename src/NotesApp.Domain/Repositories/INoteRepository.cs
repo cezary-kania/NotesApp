56f67bb631fd7526a8f2af41332bf7d17a85a1e6
cezary-kania <cezary.kaniadev@gmail.com>
2021-02-08 15:42:55 +0100

    Add note repository interface

---
+++ src/NotesApp.Domain/Repositories/INoteRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NotesApp.Domain.Entities;
+
+namespace NotesApp.Domain.Repositories
+{
+    public interface INoteRepository
+    {
+        Task<Note> GetAsync(Guid id);
+        Task<Note> GetAsync(string title);
+        Task<IEnumerable<Note>> GetAllAsync();
+        Task CreateAsync(Note note);
+        Task RemoveAsync(Guid noteId);
+        Task UpdateAsync(Note note);
+    }
+}
