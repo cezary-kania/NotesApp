7011717d6ea91b64ce12b249af01f64f023180c8
cezary-kania <cezary.kaniadev@gmail.com>
2021-02-09 09:12:06 +0100

    Remove noteId from NoteVersionDto

--- src/NotesApp.Application/DTOs/NoteVersionDto.cs
+++ src/NotesApp.Application/DTOs/NoteVersionDto.cs
@@ -4,7 +4,6 @@ namespace NotesApp.Application.DTOs
 {
     public class NoteVersionDto
     {
-        public Guid NoteId { get; set; }
         public int VersionNo { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
