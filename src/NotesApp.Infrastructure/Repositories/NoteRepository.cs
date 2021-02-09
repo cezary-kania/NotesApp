f5f74376f64d71c9a746632cd600b316690ff4ef
cezary-kania <cezary.kaniadev@gmail.com>
2021-02-09 09:20:12 +0100

    Add note repository

---
+++ src/NotesApp.Infrastructure/Repositories/NoteRepository.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NotesApp.Domain.Entities;
+using NotesApp.Domain.Repositories;
+using NotesApp.Infrastructure.EntityFramework;
+
+namespace NotesApp.Infrastructure.Repositories
+{
+    public class NoteRepository : INoteRepository
+    {
+        private readonly AppDbContext _context;
+
+        public NoteRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Note>> GetAllAsync()
+            => await _context.Notes
+                .Include(n => n.Versions)
+                .ToListAsync();
+
+        public async Task<Note> GetAsync(Guid id)
+            => await _context.Notes
+                .Include(n => n.Versions)
+                .SingleOrDefaultAsync(note => note.Id == id);
+
+        public async Task<Note> GetAsync(string title)
+            => await _context.Notes.SingleOrDefaultAsync(note => note.Modified == DateTime.UtcNow); 
+        
+        public async Task CreateAsync(Note note)
+        {
+            await _context.Notes.AddAsync(note);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Note note)
+        {
+            _context.Notes.Update(note);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
