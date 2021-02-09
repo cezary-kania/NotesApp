4d1ac65e8bc3e4c4d59d959dda22262093a45d70
cezary-kania <cezary.kaniadev@gmail.com>
2021-02-09 09:19:06 +0100

    Add database support

---
+++ src/NotesApp.Infrastructure/EntityFramework/AppDbContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using NotesApp.Domain.Entities;
+
+namespace NotesApp.Infrastructure.EntityFramework
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<NoteVersion> NoteVersions { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var notesBuilder = modelBuilder.Entity<Note>();
+            notesBuilder.HasKey(n => n.Id);            
+            var notesVersionBuilder = modelBuilder.Entity<NoteVersion>();
+            notesVersionBuilder.HasKey(nv => new {nv.NoteId, nv.VersionNo});
+        }
+    }
+}
